de27fbf43e0c8e4ecafccc05fb6b08466845cf1f
NyRoo12 <nicolas.donoso@alumnos.uach.cl>
2024-09-03 14:31:58 -0400

    Electron-React-Tailwindcss

---
+++ SistemaGestionAsignaturasIPC/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen max-h-full bg-red-500">
+      <div className="font-bold text-white">tontos</div>
+    </div>
+  );
+}
