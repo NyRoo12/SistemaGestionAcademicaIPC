2378a78cd23a538a52b1bac7c98d223b860ab5cd
Crixtianh <cristian.huenchullanca1@gmail.com>
2024-09-05 14:09:50 -0400

    Links funcionales

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-const root = createRoot(document.body);
+// Asegúrate de que el ID de 'root' coincida con el contenedor en tu archivo HTML
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(<App />);
