79369bc9f68d12a131e17c525937a2f63d66b2cf
elSebee <sebastian.anazco@alumnos.uach.cl>
2024-09-10 14:47:26 -0300

    backend

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config(); // <-- Carga las variables de entorno
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,  // Cambia según tu configuración
+    user: process.env.DB_USER,       // Tu usuario de MySQL
+    password: process.env.DB_PASSWORD,  // Tu contraseña de MySQL
+    database: process.env.DB_NAME  // Nombre de tu base de datos
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error conectando a la base de datos:', err);
+        return;
+    }
+    console.log('Conexión exitosa a la base de datos MySQL');
+});
+
+module.exports = connection;
