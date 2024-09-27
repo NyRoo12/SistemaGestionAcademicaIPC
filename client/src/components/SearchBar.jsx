123ba4ce52bd2907b4707c849c4fb22a19bad5bb
elSebee <sebastian.anazco@alumnos.uach.cl>
2024-09-27 16:27:41 -0300

    webMain

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -4,38 +4,60 @@ import ipc from "./images/ipc.png";
 
 const SearchBar = () => {
   const [query, setQuery] = useState("");
-  const [data, setData] = useState([]); // Agrega esta línea para definir `data`
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchPerformed, setSearchPerformed] = useState(false); // Nueva bandera
 
-  const handleSearch = (e) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3001/api/estudiantes`);
+        if (!response.ok) {
+          throw new Error("Error en la solicitud");
+        }
+        const result = await response.json();
+        setData(result);
+        setFilteredData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const handleSearch = async (e) => {
     e.preventDefault();
-    console.log("Buscando:", query);
-    fetchData(query);
-  };
-
-  const fetchData = async (query) => {
+    setSearchPerformed(true); // Marca que se realizó una búsqueda
     try {
       const response = await fetch(
-        `http://localhost:3001/api/estudiantes/buscar?query=${encodeURIComponent(
-          query
-        )}`
+        `http://localhost:3001/api/estudiantes/buscar?query=${encodeURIComponent(query)}`
       );
-
       if (!response.ok) {
         throw new Error("Error en la solicitud");
       }
       const result = await response.json();
-      console.log("Datos recibidos:", result); // Agrega esto para inspeccionar los datos
-      setData(result); // Guarda los datos en el estado
+      filterData(result);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
+  const filterData = (fetchedData) => {
+    const isNumber = !isNaN(query.charAt(0));
+    const result = fetchedData.filter((item) => {
+      if (isNumber) {
+        return item.rut.replace(/[^0-9]/g, '').charAt(0) === query.charAt(0);
+      } else {
+        return item.nombre.toLowerCase().charAt(0) === query.toLowerCase().charAt(0);
+      }
+    });
+    setFilteredData(result);
+  };
+
   return (
     <div className="flex flex-col items-center justify-start min-h-screen bg-gray-100 py-8">
-      {/* Alineación hacia la parte superior con justify-start y padding reducido */}
       <h1 className="text-3xl font-bold mb-4">
-        <img src={ipc} className="w-40 h-auto" />
+        <img src={ipc} className="w-40 h-auto" alt="Logo IPC" />
       </h1>
       <form onSubmit={handleSearch} className="w-full max-w-md mb-4">
         <div className="relative flex items-center w-full">
@@ -67,32 +89,41 @@ const SearchBar = () => {
           </button>
         </div>
       </form>
-      {/* Resultados de búsqueda */}
+
       <div className="w-full max-w-md">
-        {data.map((estudiante) => (
-          <Link
-            key={estudiante.rut} // Cambia estudiante.id por estudiante.rut si es necesario
-            to={`/estudiante/${estudiante.rut}`} // Ajusta la ruta según tu estructura
-            className="block p-4 mb-4 bg-gray-200 rounded-lg flex items-center hover:bg-gray-300 transition duration-300"
-          >
-            {/* Icono de usuario */}
-            <div className="flex items-center justify-center w-10 h-10 bg-gray-300 rounded-full mr-4">
-              <svg
-                className="w-6 h-6 text-gray-700"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 12c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm0 2c-3.33 0-10 1.67-10 5v2h20v-2c0-3.33-6.67-5-10-5z" />
-              </svg>
-            </div>
-            {/* Nombre y RUT del estudiante */}
-            <div className="flex flex-col">
-              <span className="text-lg">{estudiante.nombre}</span>
-              <span className="text-sm text-gray-600">{estudiante.rut}</span>
-            </div>
-          </Link>
-        ))}
+        {searchPerformed && filteredData.length === 0 ? (
+          <div className="text-center my-4">
+            <span className="text-lg text-gray-600">Sin Resultados</span>
+          </div>
+        ) : (
+          filteredData.map((estudiante) => (
+            <Link
+              key={estudiante.rut}
+              to={`/estudiante/${estudiante.rut}`}
+              className="block p-4 mb-4 bg-gray-200 rounded-lg flex items-center justify-between hover:bg-gray-300 transition duration-300"
+            >
+              <div className="flex items-center">
+                <div className="flex items-center justify-center w-10 h-10 bg-gray-300 rounded-full mr-4">
+                  <svg
+                    className="w-6 h-6 text-gray-700"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 12c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm0 2c-3.33 0-10 1.67-10 5v2h20v-2c0-3.33-6.67-5-10-5z" />
+                  </svg>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-lg">{estudiante.nombre}</span>
+                  <span className="text-sm text-gray-600">{estudiante.rut}</span>
+                </div>
+              </div>
+              <div className="text-right">
+                <span className="text-sm text-gray-500">{estudiante.carreraDestino}</span>
+              </div>
+            </Link>
+          ))
+        )}
       </div>
     </div>
   );
